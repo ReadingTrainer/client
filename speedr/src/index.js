84a6a0451c554fd5ab81c188ea5cd4b405d8ad1c
BenjaminGrabow <codingbenjamingrabow@gmx.de>
2019-10-31 12:29:20 +0100

    connect redux and react router with the app

--- speedr/src/index.js
+++ speedr/src/index.js
@@ -1,6 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter as Router } from "react-router-dom";
+import { applyMiddleware, createStore } from "redux";
+import { Provider } from "react-redux";
+import logger from 'redux-logger';
+import thunk from "redux-thunk";
+import rootReducer from "./store/reducers";
+import App from "./App";
+import "./index.css";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+
+const store = createStore(rootReducer, applyMiddleware(logger, thunk));
+
+
+ReactDOM.render(
+  <Provider store={store}>
+    <Router>
+      <App />
+    </Router>
+  </Provider>,
+  document.getElementById("root")
+);
