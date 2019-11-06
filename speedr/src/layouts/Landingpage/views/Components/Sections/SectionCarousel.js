a9a7fcb6d2b49dff48dc3bad8c19c88d2d16769f
BenjaminGrabow <codingbenjamingrabow@gmx.de>
2019-11-06 11:28:29 +0100

    add header for caroussel

--- speedr/src/layouts/Landingpage/views/Components/Sections/SectionCarousel.js
+++ speedr/src/layouts/Landingpage/views/Components/Sections/SectionCarousel.js
@@ -2,14 +2,16 @@ import React from "react";
 // react component for creating beautiful carousel
 import Carousel from "react-slick";
 // @material-ui/core components
-import { makeStyles } from "@material-ui/core/styles";
+// import { makeStyles } from "@material-ui/core/styles";
 // @material-ui/icons
 import LocationOn from "@material-ui/icons/LocationOn";
 // core components
 import GridContainer from "../../../components/Grid/GridContainer.js";
 import GridItem from "../../../components/Grid/GridItem.js";
 import Card from "../../../components/Card/Card.js";
+import Typography from "@material-ui/core/Typography";
 
+// Pictures
 // import image1 from "../../../assets/img/bg.jpg";
 // import image2 from "../../../assets/img/bg2.jpg";
 // import image3 from "../../../assets/img/bg3.jpg";
@@ -20,10 +22,12 @@ import image4 from "./abraham.jpg";
 import image5 from "./harvard.jpg";
 import image6 from "./cambridge.jpg";
 import styled from "styled-components";
+import { withStyles } from "@material-ui/styles";
+import PropTypes from "prop-types";
 
 import styles from "../../../assets/jss/material-kit-react/views/componentsSections/carouselStyle.js";
 
-const useStyles = makeStyles(styles);
+// const useStyles = makeStyles(styles);
 
 const StyledSectionCaroussel = styled.div`
 
@@ -38,19 +42,145 @@ img {
 }
 `; 
 
-export default function SectionCarousel() {
-  const classes = useStyles();
-  const settings = {
-    // dots: true,
-    infinite: true,
-    speed: 500,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    autoplay: true
-  };
-  return (
-    <StyledSectionCaroussel className={classes.section}>
+// export default function SectionCarousel() {
+//   const classes = useStyles();
+//   const settings = {
+//     // dots: true,
+//     infinite: true,
+//     speed: 500,
+//     slidesToShow: 1,
+//     slidesToScroll: 1,
+//     autoplay: true
+//   };
+//   return (
+//     <StyledSectionCaroussel className={classes.section}>
+//       <div className={classes.container}>
+//       {/* <Typography
+//           component="h1"
+//           variant="h2"
+//           align="center"
+//           color="textPrimary"
+//           gutterBottom
+//         >
+//         </Typography> */}
+//         <Typography
+//           variant="h3"
+//           align="center"
+//           color="textSecondary"
+//           component="p"
+//         >
+//           Speed reader and Universities
+//         </Typography>
+//         <GridContainer>
+//           <GridItem xs={12} sm={12} md={8} className={classes.marginAuto}>
+//             <Card carousel>
+//               <Carousel {...settings}>
+//                 <div className="picture">
+//                   <img src={image1} alt="First slide" className="slick-image" />
+//                   <div className="slick-caption">
+//                     <h4>
+//                       <LocationOn className="slick-icons" />
+//                       George Washington, United States
+//                     </h4>
+//                   </div>
+//                 </div>
+//                 <div className="picture">
+//                   <img
+//                     src={image2}
+//                     alt="Second slide"
+//                     className="slick-image"
+//                   />
+//                   <div className="slick-caption">
+//                     <h4>
+//                       <LocationOn className="slick-icons" />
+//                       President John F. Kennedy, United States
+//                     </h4>
+//                   </div>
+//                 </div>
+//                 <div className="picture">
+//                   <img src={image3} alt="Third slide" className="slick-image" />
+//                   <div className="slick-caption">
+//                     <h4>
+//                       <LocationOn className="slick-icons" />
+//                       President Jimmy Carter, United States
+//                     </h4>
+//                   </div>
+//                 </div>
+//                 <div className="picture">
+//                   <img src={image4} alt="First slide" className="slick-image" />
+//                   <div className="slick-caption">
+//                     <h4>
+//                       <LocationOn className="slick-icons" />
+//                       Abraham Lincoln, United States
+//                     </h4>
+//                   </div>
+//                 </div>
+//                 <div className="picture">
+//                   <img src={image5} alt="First slide" className="slick-image" />
+//                   <div className="slick-caption">
+//                     <h4>
+//                       <LocationOn className="slick-icons" />
+//                       Harvard University, United States
+//                     </h4>
+//                   </div>
+//                 </div><div className="picture">
+//                   <img src={image6} alt="First slide" className="slick-image" />
+//                   <div className="slick-caption">
+//                     <h4>
+//                       <LocationOn className="slick-icons" />
+//                       Cambridge University, England
+//                     </h4>
+//                   </div>
+//                 </div>
+//               </Carousel>
+//             </Card>
+//           </GridItem>
+//         </GridContainer>
+//       </div>
+//     </StyledSectionCaroussel>
+//   );
+// }
+
+class SectionCaroussel extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {  }
+  }
+
+  componentDidMount = () => {
+    document.getElementsByClassName("slick-arrow slick-prev").style.display = "none";
+  }
+
+  render() { 
+    const settings = {
+      // dots: true,
+      infinite: true,
+      speed: 500,
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      autoplay: true
+    };
+
+    const { classes } = this.props;
+    
+    return (     <StyledSectionCaroussel className={classes.section}>
       <div className={classes.container}>
+      {/* <Typography
+          component="h1"
+          variant="h2"
+          align="center"
+          color="textPrimary"
+          gutterBottom
+        >
+        </Typography> */}
+        <Typography
+          variant="h3"
+          align="center"
+          color="textSecondary"
+          component="p"
+        >
+          Speed reader and Universities
+        </Typography>
         <GridContainer>
           <GridItem xs={12} sm={12} md={8} className={classes.marginAuto}>
             <Card carousel>
@@ -117,6 +247,13 @@ export default function SectionCarousel() {
           </GridItem>
         </GridContainer>
       </div>
-    </StyledSectionCaroussel>
-  );
+    </StyledSectionCaroussel> );
+  }
 }
+
+
+SectionCaroussel.propTypes = {
+  classes: PropTypes.object.isRequired
+};
+
+export default withStyles(styles)(SectionCaroussel);
