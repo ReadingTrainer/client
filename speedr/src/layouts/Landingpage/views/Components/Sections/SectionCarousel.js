3f514de32d695e73dee2c6896e1ad9391d21709d
Talent Antonio <antonio.talenty@gmail.com>
2019-11-07 11:17:32 +0000

    favicon added

--- speedr/src/layouts/Landingpage/views/Components/Sections/SectionCarousel.js
+++ speedr/src/layouts/Landingpage/views/Components/Sections/SectionCarousel.js
@@ -1,157 +1,61 @@
-import React from "react";
+import React from 'react';
 // react component for creating beautiful carousel
-import Carousel from "react-slick";
+import Carousel from 'react-slick';
 // @material-ui/core components
 // import { makeStyles } from "@material-ui/core/styles";
 // @material-ui/icons
-import LocationOn from "@material-ui/icons/LocationOn";
+import LocationOn from '@material-ui/icons/LocationOn';
 // core components
-import GridContainer from "../../../components/Grid/GridContainer.js";
-import GridItem from "../../../components/Grid/GridItem.js";
-import Card from "../../../components/Card/Card.js";
-import Typography from "@material-ui/core/Typography";
+import GridContainer from '../../../components/Grid/GridContainer.js';
+import GridItem from '../../../components/Grid/GridItem.js';
+import Card from '../../../components/Card/Card.js';
+import Typography from '@material-ui/core/Typography';
 
 // Pictures
 // import image1 from "../../../assets/img/bg.jpg";
 // import image2 from "../../../assets/img/bg2.jpg";
 // import image3 from "../../../assets/img/bg3.jpg";
-import image1 from "./george.jpg";
-import image2 from "./john.jpg";
-import image3 from "./jimmy.jpg";
-import image4 from "./abraham.jpg";
-import image5 from "./harvard.jpg";
-import image6 from "./cambridge.jpg";
-import styled from "styled-components";
-import { withStyles } from "@material-ui/styles";
-import PropTypes from "prop-types";
+import image1 from './george.jpg';
+import image2 from './john.jpg';
+import image3 from './jimmy.jpg';
+import image4 from './abraham.jpg';
+import image5 from './harvard.jpg';
+import image6 from './cambridge.jpg';
+import styled from 'styled-components';
+import { withStyles } from '@material-ui/styles';
+import PropTypes from 'prop-types';
 
-import styles from "../../../assets/jss/material-kit-react/views/componentsSections/carouselStyle.js";
+import styles from '../../../assets/jss/material-kit-react/views/componentsSections/carouselStyle.js';
 
 // const useStyles = makeStyles(styles);
 
 const StyledSectionCaroussel = styled.div`
-
-img {
+  img {
     max-width: 100%;
     max-height: 100%;
-}
+  }
+  .haha {
+    width: 100%;
+    display: block;
+  }
 
-.picture {
-  height: 300px;
-  width: 80%;
-}
-`; 
-
-// export default function SectionCarousel() {
-//   const classes = useStyles();
-//   const settings = {
-//     // dots: true,
-//     infinite: true,
-//     speed: 500,
-//     slidesToShow: 1,
-//     slidesToScroll: 1,
-//     autoplay: true
-//   };
-//   return (
-//     <StyledSectionCaroussel className={classes.section}>
-//       <div className={classes.container}>
-//       {/* <Typography
-//           component="h1"
-//           variant="h2"
-//           align="center"
-//           color="textPrimary"
-//           gutterBottom
-//         >
-//         </Typography> */}
-//         <Typography
-//           variant="h3"
-//           align="center"
-//           color="textSecondary"
-//           component="p"
-//         >
-//           Speed reader and Universities
-//         </Typography>
-//         <GridContainer>
-//           <GridItem xs={12} sm={12} md={8} className={classes.marginAuto}>
-//             <Card carousel>
-//               <Carousel {...settings}>
-//                 <div className="picture">
-//                   <img src={image1} alt="First slide" className="slick-image" />
-//                   <div className="slick-caption">
-//                     <h4>
-//                       <LocationOn className="slick-icons" />
-//                       George Washington, United States
-//                     </h4>
-//                   </div>
-//                 </div>
-//                 <div className="picture">
-//                   <img
-//                     src={image2}
-//                     alt="Second slide"
-//                     className="slick-image"
-//                   />
-//                   <div className="slick-caption">
-//                     <h4>
-//                       <LocationOn className="slick-icons" />
-//                       President John F. Kennedy, United States
-//                     </h4>
-//                   </div>
-//                 </div>
-//                 <div className="picture">
-//                   <img src={image3} alt="Third slide" className="slick-image" />
-//                   <div className="slick-caption">
-//                     <h4>
-//                       <LocationOn className="slick-icons" />
-//                       President Jimmy Carter, United States
-//                     </h4>
-//                   </div>
-//                 </div>
-//                 <div className="picture">
-//                   <img src={image4} alt="First slide" className="slick-image" />
-//                   <div className="slick-caption">
-//                     <h4>
-//                       <LocationOn className="slick-icons" />
-//                       Abraham Lincoln, United States
-//                     </h4>
-//                   </div>
-//                 </div>
-//                 <div className="picture">
-//                   <img src={image5} alt="First slide" className="slick-image" />
-//                   <div className="slick-caption">
-//                     <h4>
-//                       <LocationOn className="slick-icons" />
-//                       Harvard University, United States
-//                     </h4>
-//                   </div>
-//                 </div><div className="picture">
-//                   <img src={image6} alt="First slide" className="slick-image" />
-//                   <div className="slick-caption">
-//                     <h4>
-//                       <LocationOn className="slick-icons" />
-//                       Cambridge University, England
-//                     </h4>
-//                   </div>
-//                 </div>
-//               </Carousel>
-//             </Card>
-//           </GridItem>
-//         </GridContainer>
-//       </div>
-//     </StyledSectionCaroussel>
-//   );
-// }
+  // .picture {
+  //   height: 10rem;
+  //   width: 700px;
+  // }
+`;
 
 class SectionCaroussel extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {  }
+    this.state = {};
   }
 
   // componentDidMount = () => {
   //   document.getElementsByClassName("slick-arrow slick-prev").style.display = "none";
   // }
 
-  render() { 
+  render() {
     const settings = {
       dots: true,
       infinite: true,
@@ -162,10 +66,11 @@ class SectionCaroussel extends React.Component {
     };
 
     const { classes } = this.props;
-    
-    return (     <StyledSectionCaroussel className={classes.section}>
-      <div className={classes.container}>
-      {/* <Typography
+
+    return (
+      <StyledSectionCaroussel className={classes.section}>
+        <div className={classes.container}>
+          {/* <Typography
           component="h1"
           variant="h2"
           align="center"
@@ -173,84 +78,110 @@ class SectionCaroussel extends React.Component {
           gutterBottom
         >
         </Typography> */}
-        <Typography
-          variant="h3"
-          align="center"
-          color="textSecondary"
-          component="p"
-        >
-          Speed reader and Universities
-        </Typography>
-        <GridContainer>
-          <GridItem xs={12} sm={12} md={8} className={classes.marginAuto}>
-            <Card carousel>
-              <Carousel {...settings}>
-                <div className="">
-                  <img src={image1} alt="First slide" className="slick-image" />
-                  <div className="slick-caption">
-                    <h4>
-                      <LocationOn className="slick-icons" />
-                      George Washington, United States
-                    </h4>
+          <Typography
+            variant="h5"
+            align="center"
+            color="textSecondary"
+            component="p"
+          >
+        Universities & People who have used Speed Readers
+          </Typography>
+          <GridContainer>
+            <GridItem xs={12} sm={12} md={8} className={classes.marginAuto}>
+              <Card className="haha" carousel>
+                <Carousel {...settings}>
+                  <div className="picture">
+                    <img
+                      src={image1}
+                      alt="First slide"
+                      className="slick-image"
+                    />
+                    <div className="slick-caption">
+                      <h4>
+                        <LocationOn className="slick-icons" />
+                        George Washington, United States
+                      </h4>
+                    </div>
                   </div>
-                </div>
-                <div className="picture">
-                  <img
-                    src={image2}
-                    alt="Second slide"
-                    className="slick-image"
-                  />
-                  <div className="slick-caption">
-                    <h4>
-                      <LocationOn className="slick-icons" />
-                      President John F. Kennedy, United States
-                    </h4>
+                  <div className="picture">
+                    <img
+                      src={image2}
+                      alt="Second slide"
+                      className="slick-image"
+                    />
+                    <div className="slick-caption">
+                      <h4>
+                        <LocationOn className="slick-icons" />
+                        President John F. Kennedy, United States
+                      </h4>
+                    </div>
                   </div>
-                </div>
-                <div className="picture">
-                  <img src={image3} alt="Third slide" className="slick-image" />
-                  <div className="slick-caption">
-                    <h4>
-                      <LocationOn className="slick-icons" />
-                      President Jimmy Carter, United States
-                    </h4>
+                  <div className="picture">
+                    <img
+                      src={image3}
+                      alt="Third slide"
+                      className="slick-image"
+                    />
+                    <div className="slick-caption">
+                      <h4>
+                        <LocationOn className="slick-icons" />
+                        President Jimmy Carter, United States
+                      </h4>
+                    </div>
                   </div>
-                </div>
-                <div className="picture">
-                  <img src={image4} alt="First slide" className="slick-image" />
-                  <div className="slick-caption">
-                    <h4>
-                      <LocationOn className="slick-icons" />
-                      Abraham Lincoln, United States
-                    </h4>
+                  <div className="picture">
+                    <img
+                      src={image4}
+                      alt="First slide"
+                      className="slick-image"
+                    />
+                    <div className="slick-caption">
+                      <h4>
+                        <LocationOn className="slick-icons" />
+                        Abraham Lincoln, United States
+                      </h4>
+                    </div>
                   </div>
-                </div>
-                <div className="picture">
-                  <img src={image5} alt="First slide" className="slick-image" />
-                  <div className="slick-caption">
-                    <h4>
-                      <LocationOn className="slick-icons" />
-                      Harvard University, United States
-                    </h4>
+                  <div className="picture">
+                    <img
+                      src={image5}
+                      alt="First slide"
+                      className="slick-image"
+                    />
+                    <div className="slick-caption">
+                      <h4>
+                        <LocationOn className="slick-icons" />
+                        Harvard University, United States
+                      </h4>
+                    </div>
                   </div>
-                </div><div className="picture">
-                  <img src={image6} alt="First slide" className="slick-image" />
-                  <div className="slick-caption">
-                    <h4>
-                      <LocationOn className="slick-icons" />
-                      Cambridge University, England
-                    </h4>
+                  <div className="picture">
+                    <img
+                      src={image6}
+                      alt="First slide"
+                      className="slick-image"
+                    />
+                    <div className="slick-caption">
+                      <h4>
+                        <LocationOn className="slick-icons" />
+                        Cambridge University, England
+                      </h4>
+                    </div>
                   </div>
-                </div>
-              </Carousel>
-            </Card>
-          </GridItem>
-        </GridContainer>
-      </div>
-    </StyledSectionCaroussel> );
+
+
+                </Carousel>
+                
+              </Card>
+            </GridItem>
+            
+          </GridContainer>
+        </div>
+      
+      </StyledSectionCaroussel>
+    );
   }
 }
-
 
 SectionCaroussel.propTypes = {
   classes: PropTypes.object.isRequired
